fb869d51db57fc9f31070d398f83bb0619602abd
Pushkar Sane <78395956+SurturFTW@users.noreply.github.com>
2022-04-11 23:13:00 +0530

    Update Pract2B.java

--- Pract2B.java
+++ Pract2B.java
@@ -1,15 +1,11 @@
  class Pract2B{
     public static void main(String[] args) {
-        //binary numbers
         String a = "1001";
-        //decimal numbers
         int p = 9;
 
-        //binary to decimal
         int x = Integer.parseInt(a, 2);
         System.out.println("Decimal of " + a + " is : " + x);
 
-        //decimal to binary
         System.out.println("Binary of " + p + " is : " + Integer.toBinaryString(p));
     }
 }
